7d457c16f5c95cc42784fbc8f9bf9120523417e0
kuroazai <pyromwansa@gmail.com>
2023-06-12 10:48:56 +0100

    update

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import enum
 import config as cfg
 import argparse
 import pickle
-from conredis import ConRedis
 
 class DBConnector:
     def __init__(self, db_path):
@@ -58,6 +57,8 @@ class SeasonChanger(DBConnector):
     engine_manufacturers: str = 'Parts_Enum_EngineManufacturers'
     design_stats: str = 'Parts_DesignStatValues'
     track_perf: str = 'Races_TeamPerformance'
+    pit_crew: str = 'Staff_PitCrew_PerformanceStats'
+    team_expertise: str = 'Parts_TeamExpertise'
 
     def __init__(self, db_path: str, base_tyre_life: int, base_perf: int, tyre3set_perf_diff: float, tyre3set_life_diff: float, dirty_air: float, drs: float, slipstream: float, f1_cfg=cfg):
         super().__init__(db_path)
@@ -114,7 +115,7 @@ class SeasonChanger(DBConnector):
         self.execute_value(query, (value,))
         # set acceleration multiplier
         query = f"UPDATE {self.race_perf_table} SET MaxDRSAccelerationMultiplier = ?;"
-        value = str(self.drs * 1.05)
+        value = str(self.drs * 1.15)
         print("Accerellation set", value)
         self.execute_value(query, (value,))
         # set acceleration multiplier
@@ -269,18 +270,14 @@ class SeasonChanger(DBConnector):
 
 
     def set_driver_data(self):
-        # mapping guide
         driver_stat_dict = dict(DriverStats.__members__)
 
-        # load  json
-        dir = os.path.join(os.getcwd(), 'drivers', 'F1_22.json')
-        with open(dir, 'r') as f:
+        my_dir = os.path.join(os.getcwd(), 'drivers', 'F1_22.json')
+        with open(my_dir, 'r') as f:
             data = json.load(f)
 
         for x in data:
-
             _id = f'[DriverCode_{x["ID"].lower().capitalize()}]'
-            # get driver StaffID  from drivers_table
             query = f"SELECT StaffID FROM {self.drivers_table} WHERE DriverCode = ?;"
             staff_id = self.execute_value(query, (_id,)).fetchone()[0]
             stats = dict(list(x.items())[2:])
@@ -306,79 +303,86 @@ class SeasonChanger(DBConnector):
         pass
 
     def equal_engines(self):
-        # from engine manufacturers table select EngineDesignID
-        query = f"SELECT EngineDesignID FROM {self.engine_manufacturers};"
-        engine_design_ids = self.execute(query)
-        print('engine ids', engine_design_ids)
-        # set engine stats in parts_design table
-        # from engine manufacturers table select ErsDesignID
-        query = f"SELECT ErsDesignID FROM {self.engine_manufacturers};"
-        ers_design_ids = self.execute(query)
-        print('ers ids', ers_design_ids)
-        # from engine manufacturers table select gearboxDesignID
-        query = f"SELECT GearboxDesignID FROM {self.engine_manufacturers};"
-        gearbox_design_ids = self.execute(query)
-        print('gearbox ids', gearbox_design_ids)
-        # from engine manufacturers table select FuelDesignID
-
         base_unit_value = 100
         base_value = 1000
-        for i, x in enumerate(engine_design_ids):
-            query = "SELECT DesignID, StatID, Value, UnitValue FROM Parts_DesignStatValues WHERE DesignID = ? "
-            result = self.execute_value(query, (x[0],)).fetchall()
-            for j, y in enumerate(result):
-                # update UnitValue in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET UnitValue = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_unit_value, result[j][0], result[j][1]))
-                # update Value in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET Value = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_value, result[j][0], result[j][1]))
 
-        for i, x in enumerate(ers_design_ids):
-            query = "SELECT DesignID, StatID, Value, UnitValue FROM Parts_DesignStatValues WHERE DesignID = ? "
-            result = self.execute_value(query, (x[0],)).fetchall()
-            for j, y in enumerate(result):
-                # update UnitValue in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET UnitValue = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_unit_value, result[j][0], result[j][1]))
-                # update Value in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET Value = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_value, result[j][0], result[j][1]))
+        # Get engine design IDs
+        query = f"SELECT EngineDesignID, ErsDesignID, GearboxDesignID FROM {self.engine_manufacturers};"
+        design_ids = self.execute(query)
+        print('design ids', design_ids)
 
-        for i, x in enumerate(ers_design_ids):
-            query = "SELECT DesignID, StatID, Value, UnitValue FROM Parts_DesignStatValues WHERE DesignID = ? "
-            result = self.execute_value(query, (x[0],)).fetchall()
-            for j, y in enumerate(result):
-                # update UnitValue in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET UnitValue = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_unit_value, result[j][0], result[j][1]))
-                # update Value in Parts_DesignStatValues where DesignID = ? and StatID = ?
-                query = f"UPDATE {self.design_stats} SET Value = ? WHERE DesignID = ? and StatID = ?;"
-                self.execute_value(query, (base_value, result[j][0], result[j][1]))
+        update_queries = []
+        values = []
 
+        for design_id in design_ids:
+            engine_design_id, ers_design_id, gearbox_design_id = design_id
+
+            # Update engine design stats
+            update_queries.append(
+                f"UPDATE {self.design_stats} SET UnitValue = ?, Value = ? WHERE DesignID = ? AND StatID IN (SELECT StatID FROM Parts_DesignStatValues WHERE DesignID = ?);")
+            values.append((base_unit_value, base_value, engine_design_id, engine_design_id))
+
+            # Update ERS design stats
+            update_queries.append(
+                f"UPDATE {self.design_stats} SET UnitValue = ?, Value = ? WHERE DesignID = ? AND StatID IN (SELECT StatID FROM Parts_DesignStatValues WHERE DesignID = ?);")
+            values.append((base_unit_value, base_value, ers_design_id, ers_design_id))
+
+            # Update gearbox design stats
+            update_queries.append(
+                f"UPDATE {self.design_stats} SET UnitValue = ?, Value = ? WHERE DesignID = ? AND StatID IN (SELECT StatID FROM Parts_DesignStatValues WHERE DesignID = ?);")
+            values.append((base_unit_value, base_value, gearbox_design_id, gearbox_design_id))
+
+        for update_query, value in zip(update_queries, values):
+            print(update_query, value)
+            self.execute_value(update_query, value)
+
+    def update_design_stats(self, design_id, unit_value, value):
+        query = "SELECT DesignID, StatID FROM Parts_DesignStatValues WHERE DesignID = ?"
+        results = self.execute_value(query, (design_id,)).fetchall()
+
+        update_queries = []
+        values = []
+
+        for result in results:
+            design_id, stat_id = result
+
+            # Update UnitValue in Parts_DesignStatValues
+            update_queries.append(f"UPDATE {self.design_stats} SET UnitValue = ? WHERE DesignID = ? AND StatID = ?;")
+            values.append((unit_value, design_id, stat_id))
+
+            # Update Value in Parts_DesignStatValues
+            update_queries.append(f"UPDATE {self.design_stats} SET Value = ? WHERE DesignID = ? AND StatID = ?;")
+            values.append((value, design_id, stat_id))
+
+        self.execute_many(update_queries, values)
+        self.commit()
 
     def equal_stats(self):
-        # teams will start with a baseline equal car will be affected by development team
         base_unit_value = 25
-        base_value = 250
-        # update UnitValue Parts_DesignStatValues SET UnitValue = 90 ;
-        query = f"UPDATE {self.design_stats} SET UnitValue = ?;"
-        self.execute_value(query, (base_unit_value,))
-        # update Value Parts_DesignStatValues SET Value = 950 ;
-        query = f"UPDATE {self.design_stats} SET Value = ?;"
-        self.execute_value(query, (base_value,))
-        print("Stats equalised")
+        base_value = base_unit_value * 10
+
+        query = f"UPDATE {self.design_stats} SET UnitValue = ?, Value = ?;"
+        values = (base_unit_value, base_value)
+        self.execute_value(query, values)
+
 
     def equal_track_stats(self):
-        query = f"UPDATE {self.track_perf} SET Straights = 1.0;"
+        query = f"UPDATE {self.track_perf} SET Straights = 1.0, SlowCorners = 1.0, FastCorners = 1.0, MediumCorners = 1.0;"
         self.execute(query)
-        query = f"UPDATE {self.track_perf} SET SlowCorners = 1.0;"
+
+    def equal_pit_crew(self):
+        query = f"UPDATE {self.pit_crew} SET Val = 100.0;"
         self.execute(query)
-        query = f"UPDATE {self.track_perf} SET FastCorners = 1.0;"
+
+    def equal_expertise(self):
+        expertise = 1000
+        query = f"UPDATE {self.team_expertise} SET Expertise = {expertise}, SeasonStartExpertise = {expertise};"
         self.execute(query)
-        query = f"UPDATE {self.track_perf} SET MediumCorners = 1.0;"
+
+    def driver_buffs(self):
+        val = 100
+        query = f"UPDATE {self.drivers_table} SET Improvability = {val}, Aggression = {val};"
         self.execute(query)
-        print("Track stats equalised")
 
 
 def pack_object(obj):
@@ -388,19 +392,19 @@ def pack_object(obj):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='F1 2021 Season Changer')
     parser.add_argument('--save', type=str, default='autosave.sav', help='the name of the save to extract from and overwrite')
-    parser.add_argument('--base_tl', type=float, default=0.95, help='base tyre life')
+    parser.add_argument('--base_tl', type=float, default=1.25, help='base tyre life')
     parser.add_argument('--base_perf', type=float, default=1.0, help='base tyre performance')
     parser.add_argument('--tperf_diff', type=float, default=0.40, help='base tyre performance')
-    parser.add_argument('--tlife_diff', type=float, default=0.25, help='tyre performance difference between 3 set of tyres')
-    parser.add_argument('--dirty_air', type=float, default=0.2, help='dirty air performance reduction')
+    parser.add_argument('--tlife_diff', type=float, default=0.55, help='tyre performance difference between 3 set of tyres')
+    parser.add_argument('--dirty_air', type=float, default=0.3, help='dirty air performance reduction')
 
-    parser.add_argument('--temp_inc_rate', type=float, default=2, help='tyre temperature increase rate')
+    parser.add_argument('--temp_inc_rate', type=float, default=1.9, help='tyre temperature increase rate')
     parser.add_argument('--temp_dec_rate', type=float, default=0.01, help='tyre temperature decrease rate')
 
-    parser.add_argument('--min_extreme_wear', type=float, default=0.4, help='min tyre wear in extreme temp range')
-    parser.add_argument('--max_extreme_wear', type=float, default=0.6, help='max tyre wear in extreme temp range')
-    parser.add_argument('--min_optimal_wear', type=float, default=0.15, help='min tyre wear in optimal temp range')
-    parser.add_argument('--max_optimal_wear', type=float, default=0.30, help='max tyre wear in optimal temp range')
+    parser.add_argument('--min_extreme_wear', type=float, default=0.3, help='min tyre wear in extreme temp range')
+    parser.add_argument('--max_extreme_wear', type=float, default=1, help='max tyre wear in extreme temp range')
+    parser.add_argument('--min_optimal_wear', type=float, default=0.1, help='min tyre wear in optimal temp range')
+    parser.add_argument('--max_optimal_wear', type=float, default=0.3, help='max tyre wear in optimal temp range')
     parser.add_argument('--min_optimal_grip', type=float, default=0.65, help='min tyre grip in optimal temp range')
     parser.add_argument('--max_optimal_grip', type=float, default=0.85, help='max tyre grip in optimal temp range')
     parser.add_argument('--min_extreme_grip', type=float, default=0.45, help='min tyre grip in extreme temp range')
@@ -408,16 +412,13 @@ if __name__ == '__main__':
     parser.add_argument('--load_season', type=str, default=None, help='load a previous instance you created from redis')
     parser.add_argument('--save_season', type=str, default=None, help='save the current instance to redis')
 
-    parser.add_argument('--drs', type=float, default=1.0005, help='Drs performance')
-    parser.add_argument('--slipstream', type=float, default=1.0, help='slipstream performance')
+    parser.add_argument('--drs', type=float, default=1.05, help='Drs performance')
+    parser.add_argument('--slipstream', type=float, default=1.0005, help='slipstream performance')
     ARGS = parser.parse_args()
 
     # save folder location
     save_folder = cfg.save_folder
     db_dir = os.path.join(save_folder, 'result', 'main.db')
-
-    # conredis
-    redis = ConRedis()
 
     #xAranaktu script to unpack to extract autosave
     script_dir = os.path.join(os.getcwd(), 'utils', 'script.py')
@@ -427,19 +428,15 @@ if __name__ == '__main__':
     os.system(os_cmd)
     print("Unpacked autosave")
 
-    # example load from redis
-    if ARGS.load_season is not None:
-        season_v1 = redis.retrieve_object_from_redis('season_v1')
-    else:
-        # example
-        season_v1 = SeasonChanger(db_path=db_dir,
-                                  base_tyre_life=ARGS.base_tl,
-                                  base_perf=ARGS.base_perf,
-                                  tyre3set_perf_diff=ARGS.tperf_diff,
-                                  tyre3set_life_diff=ARGS.tlife_diff,
-                                  dirty_air=ARGS.dirty_air,
-                                  drs=ARGS.drs,
-                                  slipstream=ARGS.slipstream,)
+    # example
+    season_v1 = SeasonChanger(db_path=db_dir,
+                              base_tyre_life=ARGS.base_tl,
+                              base_perf=ARGS.base_perf,
+                              tyre3set_perf_diff=ARGS.tperf_diff,
+                              tyre3set_life_diff=ARGS.tlife_diff,
+                              dirty_air=ARGS.dirty_air,
+                              drs=ARGS.drs,
+                              slipstream=ARGS.slipstream,)
 
     # # calculate new values and assign them to the database
     season_v1.equal_stats()
@@ -456,8 +453,6 @@ if __name__ == '__main__':
     season_v1.commit()
     print("Committed changes")
     # # TODO: save season object to redis that can be later loaded if you need to roll back
-    if ARGS.save_season is not None:
-        redis.dump_object_to_redis('season_v1', season_v1)
     print("Saved season object to redis")
     # #xAranaktu script to pack back to save
     os_cmd = f'python "{script_dir}" --operation repack --result "{autosave_dir}" --input {result_dir}'
